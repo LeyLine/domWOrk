d56c9bcd0dee78a2cfdf4fea13329d64cca3bebc
Adrian Llerena <a.llerena001@gmail.com>
2018-03-07 21:44:36 -0500

    js

---
+++ js.js
@@ -0,0 +1,12 @@
+
+//EDIT ON
+ // Generates a random color in hexadecimal (ie. #62b9cc)
+function generateRandomColor() {
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+// Changes the color of the background using STYLE
+function changeBackgroundColor() {
+  var colorBg =   document.getElementById("color-overlay");
+  colorBg.style.backgroundColor = generateRandomColor();
+}
