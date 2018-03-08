cc34f3e93e51f9c575bf74fd1c9bacf1bd6da856
Adrian Llerena <a.llerena001@gmail.com>
2018-03-07 21:46:13 -0500

    js2

--- js.js
+++ js.js
@@ -10,3 +10,22 @@ function changeBackgroundColor() {
   var colorBg =   document.getElementById("color-overlay");
   colorBg.style.backgroundColor = generateRandomColor();
 }
+function changeBackgroundText() {
+  var textBg = document.getElementById("text-bg");
+
+  if (textBg.innerHTML == "RAVE") {
+    textBg.innerHTML = "OR" ;
+  } else if (textBg.innerHTML == "OR") {
+    textBg.innerHTML = "DIE" ;
+  } else if (textBg.innerHTML == "DIE") {
+    textBg.innerHTML = "RAVE" ;
+  }
+}
+
+function changeBackground() {
+  changeBackgroundColor();
+  changeBackgroundText();
+}
+
+// Run this function every 300ms
+setInterval(changeBackground, 300);
